5df4e7845705f32ee1a958c7f27b877d62838463
Łukasz Nycz <lnycz96@gmail.com>
2021-08-21 12:13:50 +0200

    Basics quick start: SKShapeNode

--- Swifty-Ninja-P23/GameScene.swift
+++ Swifty-Ninja-P23/GameScene.swift
@@ -9,8 +9,68 @@ import SpriteKit
 
 class GameScene: SKScene {
 
+    var gameScore: SKLabelNode!
+    var score = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    var livesImage = [SKSpriteNode]()
+    var lives = 3
+    
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+    
     
     override func didMove(to view: SKView) {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        background.position = CGPoint(x: 1024, y: 768)
+        background.zPosition = -1
+        background.blendMode = .replace
+        addChild(background)
+        
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+        
+        createScore()
+        createLives()
+        createSlices()
+    }
+    
+    func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        addChild(gameScore)
+        gameScore.position = CGPoint(x: 8,y: 8)
+        score = 0
+        
+    }
+    func createLives() {
+        for i in 0..<3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)),y:720)
+            addChild(spriteNode)
+            livesImage.append(spriteNode)
+            
+        }
+        
+    }
+    func createSlices() {
+        activeSliceBG = SKShapeNode()
+        activeSliceFG.zPosition = 2
+        
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 3
+        
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+        
+        activeSliceFG.strokeColor = UIColor.white
+        activeSliceFG.lineWidth = 5
+        
+        addChild(activeSliceBG)
+        addChild(activeSliceFG)
         
     }
 }
